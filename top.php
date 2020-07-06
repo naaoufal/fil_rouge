6d1e388bde14f77602a8342f4328e3fd07ee2bf8
= <=>
2020-07-06 23:38:59 +0100

    add files

---
+++ top.php
@@ -0,0 +1,46 @@
+<div class="nav-top">
+                    <nav class="navbar navbar-default">
+                    
+                    <div class="navbar-header nav_2">
+                        <button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        
+
+                    </div> 
+                    <div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+                        <ul class="nav navbar-nav ">
+                            <li class=" active"><a href="index.php" class="hyper"><span>Home</span></a></li>   
+                            
+                            <li class="">
+                                <a href="laptops.php" class="drop hyper"><span>Laptops</span></a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="mobiles.php" class="dropdown-toggle hyper"><span>Mobiles</span></a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="headset.php" class="dropdown-toggle hyper"><span>HeadSets</span></a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="workstation.php" class="dropdown-toggle hyper"><span>WorkStations</span></a>
+                            </li>
+
+
+                            <li class="dropdown">
+                                <a href="mouses.php" class="dropdown-toggle hyper"><span>Mouses</span></a>
+                            </li>
+                            
+<!--                            <li><a href="codes.php" class="hyper"> <span>FAQs</span></a></li>
+ -->                            <li><a href="contact.php" class="hyper"><span>Contact Us</span></a></li>
+                        </ul>
+                    </div>
+                    </nav>
+                     <div class="cart" >
+                    
+                        <span class="fa fa-shopping-cart my-cart-icon"><span class="badge badge-notify my-cart-badge"></span></span>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
